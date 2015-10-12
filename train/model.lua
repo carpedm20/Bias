0918e37791e80a948793e02be081c3b48efeeace
Taehoon Kim <carpedm20@gmail.com>
2015-10-12 20:20:17 +0900

    Model finished (but need debugging)

--- train/model.lua
+++ train/model.lua
@@ -133,20 +133,22 @@ function Model:new_decoder2()
   return nn.gModule(inputs, {logprobs})
 end
 
-function Model:train(dataset)
+function Model:train(data)
   -- nn:training() : This sets the mode of the Module (or sub-modules) to train=true. This is useful for modules like Dropout that have a different behaviour during training vs evaluation.
   -- nn:evaluate() : This sets the mode of the Module (or sub-modules) to train=false.
+  assert(x:size(1) == y:size(1), "Dimensions of x (" .. x:size(1) .. ") and y (" .. y:size(1) .. ")  are different")
+
   self.lstm:training()
   self.decoder:training()
   if self.structure == 'bilstm' then
     self.lstm_b:training()
   end
 
-  local indicies = torch.randperm(dataset.size)
+  local indicies = torch.randperm(data.y:size() - self.window_size + 1)
   local zeros = torch.zeros(self.mem_dim)
-  for i=1, dataset.size, self.batch_size do
-    xlua.progress(i, dataset.size)
-    local batch_size = math.min(i+self.batch_size-1, dataset.size)-i+1
+  for i=1, data.x:size(1), self.batch_size do
+    xlua.progress(i, data.x:size(1))
+    local batch_size = math.min(i+self.batch_size-1, data.x:size(1))-i+1
 
     local feval = function(x)
       self.grad_params:zero()
@@ -201,35 +203,135 @@ function Model:train(dataset)
     optim.adagrad(feval, self.params, self.optim_state)
     self.emb:updateParameters(self.emb_learning_rate)
   end
-  xlua.progress(dataset.size, dataset.size)
+  xlua.progress(data.x:size(1), data.x:size(1))
 end
 
-function Model:LSTM_backward(x, inputs, rep_grad)
+function Model:LSTM_backward(sent, inputs, rep_grad)
   local grad
   if self.num_layers == 1 then
-    gard = torch.zeros(x:nElement(), self.mem_dim)
-    grad[x:nElement()] = rep_grad
+    grad = torch.zeros(sent:nElement(), self.mem_dim)
+    grad[sent:nElement()] = rep_grad
   else
-    grad = torch.zeros(x:nElement(), self.num_layers, self.mem_dim)
-    for l=1, self.num_layers do
-      grad[{x, l, {}}] = rep_gard[l]
+    grad = torch.zeros(sent:nElement(), self.num_layers, self.mem_dim)
+    for l = 1, self.num_layers do
+      grad[{sent:nElement(), l, {}}] = rep_grad[l]
     end
   end
-  local input_grads = self.lstm:backward(inputs,grad)
+  local input_grads = self.lstm:backward(inputs, grad)
   return input_grads
 end
 
-function Model:LSTM_backward(x, inputs, rep_grad)
-  local grad
+-- Bidirectional LSTM backward propagation
+function Model:BiLSTM_backward(sent, inputs, rep_grad)
+  local grad, grad_b
   if self.num_layers == 1 then
-    gard = torch.zeros(x:nElement(), self.mem_dim)
-    grad[x:nElement()] = rep_grad
+    grad   = torch.zeros(sent:nElement(), self.mem_dim)
+    grad_b = torch.zeros(sent:nElement(), self.mem_dim)
+    grad[sent:nElement()] = rep_grad[1]
+    grad_b[1] = rep_grad[2]
   else
-    grad = torch.zeros(x:nElement(), self.num_layers, self.mem_dim)
-    for l=1, self.num_layers do
-      grad[{x, l, {}}] = rep_gard[l]
+    grad   = torch.zeros(sent:nElement(), self.num_layers, self.mem_dim)
+    grad_b = torch.zeros(sent:nElement(), self.num_layers, self.mem_dim)
+    for l = 1, self.num_layers do
+      grad[{sent:nElement(), l, {}}] = rep_grad[1][l]
+      grad_b[{1, l, {}}] = rep_grad[2][l]
     end
   end
-  local input_grads = self.lstm:backward(inputs,grad)
-  return input_grads
+  local input_grads = self.lstm:backward(inputs, grad)
+  local input_grads_b = self.lstm_b:backward(inputs, grad_b, true)
+  return input_grads + input_grads_b
+end
+
+function Model:predict(x)
+  self.lstm:evaluate()
+  self.decoder:evaluate()
+  local inputs = self.emb:forward(x)
+
+  local rep
+  if self.structure == 'lstm' then
+    rep = self.lstm:forward(inputs)
+  elseif self.structure == 'bilstm' then
+    self.lstm_b:evaluate()
+    rep = {
+      self.lstm:forward(inputs),
+      self.lstm_b:forward(inputs, true),
+    }
+  end
+  local logprobs = self.decoder:forward(rep)
+  local prediction
+  -- if self.fine_grained then
+  prediction = argmax(logprobs)
+  -- else
+  --   prediction = (logprobs[1] > logprobs[3]) and 1 or 3
+  -- end
+  self.lstm:forget()
+  if self.structure == 'bilstm' then
+    self.lstm_b:forget()
+  end
+  return prediction
+end
+
+-- Produce sentiment predictions for each sentence in the dataset.
+function Model:predict_dataset(data)
+  local predictions = torch.Tensor(data.x:size(1))
+  for i = 1, data.x:size(1) do
+    xlua.progress(i, data.x:size(1))
+    local x = data.x[{{idx, idx+self.window_size}}]
+    predictions[i] = self:predict(x)
+  end
+  return predictions
+end
+
+function argmax(v)
+  local idx = 1
+  local max = v[1]
+  for i = 2, v:size(1) do
+    if v[i] > max then
+      max = v[i]
+      idx = i
+    end
+  end
+  return idx
+end
+
+function Model:print_config()
+  local num_params = self.params:size(1)
+  local num_decoder_params = self:new_decoder():getParameters():size(1)
+  printf('%-25s = %d\n',   'num params', num_params)
+  printf('%-25s = %d\n',   'num compositional params', num_params - num_decoder_params)
+  printf('%-25s = %d\n',   'word vector dim', self.emb_dim)
+  printf('%-25s = %d\n',   'LSTM memory dim', self.mem_dim)
+  printf('%-25s = %s\n',   'LSTM structure', self.structure)
+  printf('%-25s = %d\n',   'LSTM layers', self.num_layers)
+  printf('%-25s = %.2e\n', 'regularization strength', self.reg)
+  printf('%-25s = %d\n',   'minibatch size', self.batch_size * (self.train_subtrees + 1))
+  printf('%-25s = %.2e\n', 'learning rate', self.learning_rate)
+  printf('%-25s = %.2e\n', 'word vector learning rate', self.emb_learning_rate)
+  printf('%-25s = %s\n',   'dropout', tostring(self.dropout))
+end
+
+function Model:save(path)
+  local config = {
+    batch_size        = self.batch_size,
+    dropout           = self.dropout,
+    emb_learning_rate = self.emb_learning_rate,
+    emb_vecs          = self.emb.weight:float(),
+    learning_rate     = self.learning_rate,
+    num_layers        = self.num_layers,
+    mem_dim           = self.mem_dim,
+    reg               = self.reg,
+    structure         = self.structure,
+  }
+
+  torch.save(path, {
+    params = self.params,
+    config = config,
+  })
+end
+
+function Model.load(path)
+  local state = torch.load(path)
+  local model = treelstm.Model.new(state.config)
+  model.params:copy(state.params)
+  return model
 end
